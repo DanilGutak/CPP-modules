f7b214fa830c0e57e342045e31b87f5ecbb8b1e9
DanilGutak <danil.gutak@rbinternational.com>
2023-12-06 18:01:11 +0100

    check

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -2,6 +2,16 @@
 
 int main(void)
 {
-    std::string lol = "HI THIS IS BRAIN";
+    std::string str = "HI THIS IS BRAIN";
+    std::string& stringREF = str;
+    std::string *stringPTR = &str;
     
+    std::cout << &str << "\n";
+    std::cout << &stringREF << "\n";
+    std::cout << &stringPTR << "\n\n";
+
+    std::cout << str << "\n";
+    std::cout << stringREF << "\n";
+    std::cout << *stringPTR << "\n";
+
 }
