0551ba5ed37cfc2a86b82ad6120b821d210da9a9
DanilGutak <otecgutak@gmail.com>
2024-01-02 16:50:29 +0100

    ex04 wip

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,3 +1,16 @@
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 int main(int argc, char **argv){
-1
+    if (argc != 4)
+    {
+        std::cout << "Wrong number of arguments\n";
+        return (0);
+    }
+    
+    std::ifstream outputFile(argv[1]);
+    std::string resfile = (std::string)argv[1] + ".replace";
+    std::ofstream outputFile1(resfile.c_str());
 }
