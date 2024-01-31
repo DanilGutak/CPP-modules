e12687b52fa8125aaf8db01e7e2c430d1ee033f6
DanilGutak <otecgutak@gmail.com>
2024-01-31 19:12:00 +0100

    added ex02 cpp07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -2,14 +2,45 @@
 #include <iostream>
 #include <string>
 
-template <typename Type> void iter(const Type *array, int len, void (*f)(Type const &))
-{
-    for (int i = 0; i < len; i++)
-        f(array[i]);
-}
-
-template <typename Type> void iter(Type *array, int len, void (*f)(Type &))
-{
-    for (int i = 0; i < len; i++)
-        f(array[i]);
-}
+template <typename T>
+class Array {
+private:
+    T *array;
+    unsigned int size;
+public:
+    unsigned int size(void) const {
+        return this->size;
+    }
+    Array() {
+        this->array = NULL;
+        this->size = 0;
+    }
+    Array(unsigned int n) {
+        this->array = new T[n];
+        this->size = n;
+    }
+    Array(const Array &src) {
+        *this = src;
+    }
+     ~Array() {
+        delete[] this->array;
+    }
+    Array &operator=(const Array &src) {
+        if (this != &src) {
+            delete[] this->array;
+            this->array = new T[src.size];
+            for (unsigned int i = 0; i < src.size; i++)
+                this->array[i] = src.array[i];
+            this->size = src.size;
+        }
+        return *this;
+    }
+    T &operator[](unsigned int i) {
+        if (i >= this->size) {
+            throw std::exception();
+        }
+        return this->array[i];
+    }
+    
+   
+};
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,41 +1,28 @@
-#include "iter.hpp"
-template <typename T> void print(T const &x) {
-    std::cout << x << " ";
-}
-template <typename T> void print(T &x) {
-    std::cout << x << " ";
-}
-int main( void ) {
-    int a[10] = {7 , 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    const int b[10] = {9,8,7,6,5,4,3,2,1,0};
+# include "Array.hpp"
 
-    iter(a, 10, print);
-    std::cout << "\n";
-    iter(b, 10, print);
-    std::cout << "\n";
+int main(){
+    Array<int> a(5);
 
+    for (int i = 0; i < 5; i++)
+        a[i] = i;
+    
+    Array<int> b(a);
+    Array<int> c;
+    c = a;
+    Array<int> d(0);
 
-    char c[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-    const char d[10] = {'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-    std::cout << "\n";
+    c[0] = 42;
+    std::cout << "a: " << a[0] << std::endl;
+    std::cout << "b: " << b[0] << std::endl;
 
-    iter(c, 10, print);
-    std::cout << "\n";
-    iter(d, 10, print);
-    std::cout << "\n";
-    std::cout << "\n";
+    std::cout << "c: " << c[0] << std::endl;
 
-    std::string e[10] = {"heh", "bon", "cc", "dasd", "e21", "vf", "bgg", "fh", "ii"};
-    const std::string f[10] = {"ii", "fh", "bgg", "vf", "e21", "dasd", "cc", "bon", "heh"};
-    iter(e, 10, print);
-    std::cout << "\n";
-    iter(f, 10, print);
-    std::cout << "\n";
-    return 0;
+    try {
+        std::cout << "d: " << d[0] << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << "d: " << e.what() << std::endl;
+    }
+    int i = a.size(); 
 
-
-
-
-
-
-}
+};
