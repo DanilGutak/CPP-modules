e2f156d96e4a921859be1da5e628828fe25f15c0
DanilGutak <otecgutak@gmail.com>
2024-03-04 15:45:09 +0100

    pachka papiros

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -21,8 +21,11 @@ PmergeMe &PmergeMe::operator=( const PmergeMe & ) {
 	return (*this);
 }
 
-
+const int PmergeMe::Jacobsthal[] = { 0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461,
+ 10923, 21845, 43691, 87381, 174763, 349525, 699051, 1398101, 2796203, 5592405, 11184811, 22369621, 
+ 44739243, 89478485, 178956971, 357913941, 715827883, 1431655765, 2863311531};
 void PmergeMe::mergeinsertSort(std::vector<int>& vec) {
+
     //base case
 	if (vec.size() < 2)
 		return;
@@ -38,7 +41,7 @@ void PmergeMe::mergeinsertSort(std::vector<int>& vec) {
 	std::vector<int> right(middle, end);
 
 	// sort pairs in the left and right vectors
-	for (unsigned int i = 0; i < left.size() - 1; i++)
+	for (unsigned int i = 0; i < vec.size() / 2; i++)
 		if (left[i] > right[i])
 			std::swap(left[i], right[i]);
 	
@@ -55,11 +58,31 @@ void PmergeMe::mergeinsertSort(std::vector<int>& vec) {
 		//find ith element in right vector in the copy right vector
 		std::vector<int>::iterator it = std::find(right_copy.begin(), right_copy.end(), right[i]);
 		
+		if (it == right_copy.end())
+			continue;
+		// retrieve the index of the element in the copy right vector
+		int index = std::distance(right_copy.begin(), it);
+
+		// replace the element from the copy left vector at the index of the element in the left vector
+		std::swap(left[i], left_copy[index]);
 	}
 
+	std::vector <int> ret;
+	// add strangler to the left vector if it exists
+	if (stragler)
+	{
+		left.push_back(stragler);
+	}
+
+	ret = right;
+	// add the elements of the left vector to the right vector since the left vector is sorted and the first elem is the smallest one
+	ret.insert(ret.begin(), left[0]);
+
+	
 
 
 
+	vec = ret;
 	
 
 }
