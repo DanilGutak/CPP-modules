2160d268634745abba16998fd4b06fe67ccfad66
Danil Gutak <69326654+DanilGutak@users.noreply.github.com>
2024-03-07 18:13:43 +0000

    prichishi ebasos

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,7 +1,86 @@
 
 #include "PmergeMe.hpp"
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
 
+// Function to calculate the nth Jacobsthal number (0-indexed)
+int jacobsthal(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+}
+
+// Function to determine the order of pending elements
+std::vector<int> pending_element_order(int n) {
+    std::vector<int> order;
+    for (int i = 0; jacobsthal(i) < n; ++i)
+        order.push_back(jacobsthal(i));
+    order.push_back(n); // Add n to the order
+    return order;
+}
+
+// Binary search to find the insertion point
+template <typename T>
+int binary_search_insertion_point(const std::vector<T>& vec, const T& value) {
+    int low = 0;
+    int high = vec.size();
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (vec[mid] < value)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
+
+// Function to perform binary insertion into a sorted vector
+template <typename T>
+void binary_insert(std::vector<T>& vec, const T& value) {
+    int pos = binary_search_insertion_point(vec, value);
+    vec.insert(vec.begin() + pos, value);
+}
+
+// Merge-insertion sort function
+void merge_insertion_sort(std::vector<int>& A) {
+    std::vector<std::pair<int, int> > pairs;
+    for (size_t i = 0; i + 1 < A.size(); i += 2) {
+        if (A[i] > A[i + 1])
+            std::swap(A[i], A[i + 1]);
+        pairs.push_back(std::make_pair(A[i], A[i + 1]));
+    }
+    
+    std::sort(pairs.begin(), pairs.end());
+
+    std::vector<int> main_chain;
+    std::vector<int> pending_elements;
+    for (size_t i = 0; i < pairs.size(); ++i) {
+        main_chain.push_back(pairs[i].first);
+        pending_elements.push_back(pairs[i].second);
+    }
+
+    if (A.size() % 2 != 0)
+        pending_elements.push_back(A[A.size() - 1]);
+
+    std::vector<int> main_chain_positions(main_chain.size());
+    for (size_t i = 0; i < main_chain_positions.size(); ++i)
+        main_chain_positions[i] = i;
+
+    for (size_t i = 0; i < pending_elements.size(); ++i) {
+        int index = pending_element_order(i).back();
+        int insert_index = binary_search_insertion_point(main_chain, pending_elements[index]);
+        main_chain_positions.insert(main_chain_positions.begin() + insert_index, i);
+        main_chain.insert(main_chain.begin() + insert_index, pending_elements[i]);
+    }
+
+    A = main_chain;
+}
 int main(int argc, char **argv)
 {
     if (argc < 2)
@@ -43,7 +122,7 @@ int main(int argc, char **argv)
         vec.push_back(std::atoi(argv[i]));
     }
     clock_t start2 = clock();
-    PmergeMe::mergeinsertSort(vec);
+    merge_insertion_sort(vec);
     clock_t end = clock();
     
     double vector_time_data = (double)(end - start) / CLOCKS_PER_SEC;
@@ -56,6 +135,7 @@ int main(int argc, char **argv)
     std::cout << "After sort:" << std::endl;
     for (unsigned int i = 0; i < vec.size(); i++)
         std::cout << vec[i] << " ";
+    std::cout << std::endl;
     // time for vector
 
     // time for deque
