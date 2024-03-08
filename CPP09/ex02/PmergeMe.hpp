2df78c75173742bade2f85304022f8a52e0227c2
DanilGutak <otecgutak@gmail.com>
2024-03-08 16:12:01 +0100

    ya aleksandr gruzd

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: danil.gutak <danil.gutak@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/29 18:29:13 by danil.gutak          #+#    #+#             */
-/*   Updated: 2024/02/29 18:29:13 by danil.gutak         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #pragma once
 
 #ifndef PMERGE_ME_HPP
@@ -27,93 +15,69 @@
 int jacobsthal(int n);
 int pending_element_order(int n);
 
-// Binary search to find the insertion point
-	template <typename T>
-	int binary_search_insertion_point(const T& vec, const int& value) {
-		int low = 0;
-		int high = vec.size();
-		while (low < high) {
-			int mid = low + (high - low) / 2;
-			if (vec[mid] < value)
-				low = mid + 1;
-			else
-				high = mid;
-		}
-		return low;
-	}
-
-	// Function to perform binary insertion into a sorted vector
-	template <typename T>
-	void binary_insert(T& vec, const int& value) {
-		int pos = binary_search_insertion_point(vec, value);
-		vec.insert(vec.begin() + pos, value);
-	}
-// Recursive merge-insertion sort function for pairs
-template <typename T>
-int binary_search_insertion_point_pairs(const T& vec, const std::pair<int, int>& value) {
-    int low = 0;
-    int high = vec.size();
-    while (low < high) {
-        int mid = low + (high - low) / 2;
-        if (vec[mid].first < value.first)
-            low = mid + 1;
-        else
-            high = mid;
+int pending_element_position(unsigned int  n, int size) {
+    std::vector<int> positions;
+    for (int i = 0; jacobsthal(i) < size; ++i) {
+        positions.push_back(jacobsthal(i));
     }
-    return low;
+    if (n < positions.size()) {
+        return positions[n];
+    }
+    return size - 1; // If beyond the last Jacobsthal number, use the last element
 }
 
-// Function to perform binary insertion into a sorted vector of pairs
+// Binary search to find the insertion point
 template <typename T>
-void binary_insert_pairs(T& vec, const std::pair<int, int>& value) {
-    int pos = binary_search_insertion_point_pairs(vec, value);
-    vec.insert(vec.begin() + pos, value);
+int binary_search_insertion_point(const T& vec, const int& value) {
+	int low = 0;
+	int high = vec.size();
+	while (low < high) {
+		int mid = low + (high - low) / 2;
+		if (vec[mid] < value)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	return low;
 }
-
-// Recursive merge-insertion sort function for pairs
-void merge_insertion_sort_pairs(std::vector<std::pair<int, int> >& pairs) {
-    if (pairs.size() <= 1)
-        return;
-
-    std::vector<std::pair<int, int> > left_half(pairs.begin(), pairs.begin() + pairs.size() / 2);
-    std::vector<std::pair<int, int> > right_half(pairs.begin() + pairs.size() / 2, pairs.end());
-
-    merge_insertion_sort_pairs(left_half);
-    merge_insertion_sort_pairs(right_half);
-
-    pairs.clear();
-
-    unsigned int i = 0, j = 0;
-    while (i < left_half.size() && j < right_half.size()) {
-        if (left_half[i] < right_half[j]) {
-            binary_insert_pairs(pairs, left_half[i]);
-            ++i;
+template <typename T>
+void merge(std::vector<T>& vec, int start, int mid, int end) {
+    std::vector<T> temp;
+    int i = start, j = mid;
+    while (i < mid && j < end) {
+        if (vec[i] < vec[j]) {
+            temp.push_back(vec[i++]);
         } else {
-            binary_insert_pairs(pairs, right_half[j]);
-            ++j;
+            temp.push_back(vec[j++]);
         }
     }
-
-    while (i < left_half.size()) {
-        binary_insert_pairs(pairs, left_half[i]);
-        ++i;
+    while (i < mid) {
+        temp.push_back(vec[i++]);
     }
-
-    while (j < right_half.size()) {
-        binary_insert_pairs(pairs, right_half[j]);
-        ++j;
+    while (j < end) {
+        temp.push_back(vec[j++]);
     }
-
-    // Order pending elements
-    std::vector<std::pair<int, int> > ordered_pairs;
-    for (unsigned int k = 0; k < pairs.size(); ++k) {
-        int order = pending_element_order(k);
-        ordered_pairs.push_back(pairs[order]);
-    }
-
-    pairs = ordered_pairs;
+    std::copy(temp.begin(), temp.end(), vec.begin() + start);
 }
 
+// Recursive function to sort pairs using the Ford-Johnson merge-insertion sort algorithm
+template <typename T>
+void merge_insertion_sort_pairs(std::vector<T>& pairs, int start, int end) {
+    if (end - start <= 1) return;
+
+    int mid = start + (end - start) / 2;
+    merge_insertion_sort_pairs(pairs, start, mid);
+    merge_insertion_sort_pairs(pairs, mid, end);
+    merge(pairs, start, mid, end);
+
+    // Insert pending elements using the Jacobsthal sequence
+    for (int i = 0; i < mid - start; ++i) {
+        int position = pending_element_position(i, end - start);
+        if (position != i) {
+            std::rotate(pairs.begin() + start + i, pairs.begin() + start + position, pairs.begin() + start + position + 1);
+        }
+    }
+}
 
 // Merge-insertion sort function
 template <typename T>
@@ -125,7 +89,7 @@ void merge_insertion_sort(T &A) {
         pairs.push_back(std::make_pair(A[i], A[i + 1]));
     }
     
-    merge_insertion_sort_pairs(pairs);
+    merge_insertion_sort_pairs(pairs, 0,pairs.size());
 
     T main_chain;
     T pending_elements;
